40dfc6a342f85bfe7995e788f1e12c8d9e9219fa
subaru nara <snara@student.42tokyo.jp>
2022-02-21 01:28:34 +0900

    rename scan->parse.c

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: subaru <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 17:40:56 by subaru            #+#    #+#             */
-/*   Updated: 2022/02/20 17:00:29 by subaru           ###   ########.fr       */
+/*   Updated: 2022/02/21 00:01:32 by subaru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,16 @@
 
 #define N_FIELD 3
 
-void	scan_file(const char *filename, t_vec_array	*vecs)
+static void	expand(t_vec_array *vecs, ssize_t size)
+{
+	vecs->len += size;
+	if (vecs->len < 0)
+		vecs->len = 0;
+	vecs->ptr = (t_vec *)or_exit(
+			realloc(vecs->ptr, sizeof(*vecs->ptr) * vecs->len));
+}
+
+void	parse_file(t_vec_array *vecs, const char *filename)
 {
 	t_vec		*next;
 	FILE		*ifs;
@@ -24,13 +33,24 @@ void	scan_file(const char *filename, t_vec_array	*vecs)
 	ifs = or_exit(fopen(filename, "r"));
 	while (!feof(ifs))
 	{
-		vecs->len += 1;
-		vecs->ptr = (t_vec *)or_exit(realloc(vecs->ptr, sizeof(t_vec) * vecs->len));
+		expand(vecs, 1);
 		next = &vecs->ptr[vecs->len - 1];
 		if (fscanf(ifs, "%lf,%lf,%lf\n", &next->x, &next->y, &next->z) != N_FIELD
 			|| ferror(ifs))
-			str_exit("scanf() failed\n", EXIT_FAILURE);
+			str_exit("file format error\n", EXIT_FAILURE);
 	}
 	printf("len %zu,error %d,eof %d\n", vecs->len, ferror(ifs), feof(ifs));
 	fclose(ifs);
+}
+
+void	parse_files(t_vec_array *vecs, const char **filenames)
+{
+	size_t	i;
+
+	i = 0;
+	while (filenames[i])
+	{
+		parse_file(vecs, filenames[i]);
+		i++;
+	}
 }

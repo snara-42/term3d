0e2019412780cf3b05d06bb0547d0e468960bbd8
subaru nara <snara@student.42tokyo.jp>
2022-02-21 04:47:27 +0900

    mv enum

--- term3d.h
+++ term3d.h
@@ -6,7 +6,7 @@
 /*   By: subaru <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 17:42:02 by subaru            #+#    #+#             */
-/*   Updated: 2022/02/21 01:07:50 by subaru           ###   ########.fr       */
+/*   Updated: 2022/02/21 03:58:55 by subaru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,12 @@
 typedef struct s_screen		t_screen;
 typedef struct s_vec		t_vec;
 typedef struct s_vec_array	t_vec_array;
-typedef enum e_axis
-{
-	AXIS_X = 0,
-	AXIS_Y = 1,
-	AXIS_Z = 2,
-	N_AXIS = 3
-}	t_e_axis;
 
 void	str_exit(const char *str, int status);
 void	*or_exit(void *ptr);
 
 void	parse_files(t_vec_array *vecs, const char **filename);
+void	adjust_zoom(t_vec_array *vecs);
 void	display(t_vec_array *vecs);
-
-void	rotate(t_vec_array *vecs, double angle, t_e_axis axis);
-t_vec	vec_rot(t_vec v, double angle, t_e_axis axis);
 
 #endif /* TERM3D_H */
--- vec.h
+++ vec.h
@@ -6,7 +6,7 @@
 /*   By: subaru <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 18:24:24 by subaru            #+#    #+#             */
-/*   Updated: 2022/02/21 00:58:45 by subaru           ###   ########.fr       */
+/*   Updated: 2022/02/21 04:18:26 by subaru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 
 # include <stdio.h>
 # include <math.h>
+
+typedef enum e_axis
+{
+	AXIS_X = 0,
+	AXIS_Y = 1,
+	AXIS_Z = 2,
+	N_AXIS = 3
+}	t_e_axis;
 
 typedef struct s_vec
 {
@@ -29,14 +37,22 @@ typedef struct s_vec_array
 	size_t	len;
 }	t_vec_array;
 
+typedef double	(*t_func_d)();
+typedef t_vec	(*t_func_v)();
+
 void	vec_print(t_vec v);
 t_vec	vec_add(t_vec v1, t_vec v2);
 t_vec	vec_sub(t_vec v1, t_vec v2);
 t_vec	vec_mul(t_vec v, double m);
-double	vec_len(t_vec v);
 
+double	vec_len(t_vec v);
 double	vec_dot(t_vec v1, t_vec v2);
 t_vec	vec_cross(t_vec v1, t_vec v2);
 t_vec	vec_unit(t_vec v);
+
+void	vec_foreach(t_vec_array *vecs, t_func_v f_vec, double arg);
+double	vec_max_min(t_vec_array *vecs, t_func_d f_cmp, t_func_d f_vec);
+
+void	vec_rotate(t_vec_array *vecs, double angle, t_e_axis axis);
 
 #endif /* VEC_H */

ef55518b2f6c3be640b8269dd446d78e584a854a
subaru nara <snara@student.42tokyo.jp>
2022-02-21 01:46:13 +0900

    mkdir 3d_files; add enum, separate t_screen

--- term3d.h
+++ term3d.h
@@ -6,37 +6,39 @@
 /*   By: subaru <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 17:42:02 by subaru            #+#    #+#             */
-/*   Updated: 2022/02/20 17:04:41 by subaru           ###   ########.fr       */
+/*   Updated: 2022/02/21 01:07:50 by subaru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TERM3D_H
 # define TERM3D_H
 
-#include "vec.h"
 # include <stdio.h>
+# include <unistd.h>
 # include <stdlib.h>
 # include <stdbool.h>
 # include <string.h>
 # include <strings.h>
 # include <errno.h>
 
-# define SCREEN_X 64
-# define SCREEN_Y 32
-
-typedef struct s_screen
-{
-	size_t		size_x;
-	size_t		size_y;
-	signed char	scr[SCREEN_Y][SCREEN_X];
-}	t_screen;
-
+typedef struct s_screen		t_screen;
+typedef struct s_vec		t_vec;
 typedef struct s_vec_array	t_vec_array;
+typedef enum e_axis
+{
+	AXIS_X = 0,
+	AXIS_Y = 1,
+	AXIS_Z = 2,
+	N_AXIS = 3
+}	t_e_axis;
 
 void	str_exit(const char *str, int status);
 void	*or_exit(void *ptr);
 
-void	scan_file(const char *filename, t_vec_array	*vecs);
+void	parse_files(t_vec_array *vecs, const char **filename);
 void	display(t_vec_array *vecs);
+
+void	rotate(t_vec_array *vecs, double angle, t_e_axis axis);
+t_vec	vec_rot(t_vec v, double angle, t_e_axis axis);
 
 #endif /* TERM3D_H */

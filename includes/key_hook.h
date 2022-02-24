47a7b9d5e459a44bbcb6fb26f1704e47a760a505
mfunyu <mfunyu@student.42tokyo.jp>
2022-02-24 11:09:19 +0900

    add: check for arrow keys

--- includes/key_hook.h
+++ includes/key_hook.h
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 17:44:25 by mfunyu            #+#    #+#             */
-/*   Updated: 2022/02/24 02:27:43 by subaru           ###   ########.fr       */
+/*   Updated: 2022/02/24 10:55:51 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,20 @@
 # include "term3d.h"
 
 # define ESC '\x1b'
+# define ARROW '\x5b'
 # define ERASE_LINE "\e[2K"
 
 # define ZOOM_LIMIT 42
 # define ZOOM_SPEED 1.414213562
 # define ROTATE_SPEED 0.1
+
+typedef enum s_arrow_keys
+{
+	UP = 1,
+	DOWN,
+	LEFT,
+	RIGHT
+}			t_arrow_keys;
 
 void	key_hook(int (*funct_ptr)(), void *param1, void *param2);
 int		key_handler(int key, t_vec_array *vecs, t_screen *screen);
--- srcs/key_hook.c
+++ srcs/key_hook.c
@@ -38,15 +38,42 @@ static int	getch(void)
 	return (-1);
 }
 
+static int	get_if_arrow_key(void)
+{
+	int	tmp;
+	int	key;
+
+	key = -1;
+	tmp = getch();
+	if (tmp == ARROW)
+	{
+		tmp = getch();
+		if (tmp == 65)
+			key = UP;
+		else if (tmp == 66)
+			key = DOWN;
+		else if (tmp == 67)
+			key = RIGHT;
+		else if (tmp == 68)
+			key = LEFT;
+	}
+	return (key);
+}
+
 void	key_hook(int (*funct_ptr)(), void *param1, void *param2)
 {
 	int	key;
+	int	tmp;
 
 	key = getch();
 	if (key > 0)
 	{
-		key = getchar();
-		printf(ERASE_LINE);
+		if (key == ESC)
+		{
+			tmp = get_if_arrow_key();
+			if (tmp > 0)
+				key = tmp;
+		}
 		funct_ptr(key, param1, param2);
 	}
 }
